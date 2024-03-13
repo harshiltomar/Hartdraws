a0a078a658be184b18936cc295d72f0bab449701
Harshil Tomar <harshiltomar20@gmail.com>
2024-03-13 11:42:01 +0530

    1.0
    Landing Page Added, Auth Integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
             Get the most from your <span className="text-white">Documents</span>
           </h1>
-          <p className="mt-5 justify-center max-w-prose ml-40 text-zinc-700 sm:text-lg text-center">
+          <p className="mt-5 justify-center max-w-2xl sm:ml-40 text-zinc-700 sm:text-lg text-center">
             <span className="font-bold text-black">HART</span>draws allows you
             to have candid convo with any PDF Document. Simply upload your File
             and Start asking Your Questions !!
@@ -48,10 +48,10 @@ export default function Home() {
                 clipPath:
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/1 rotate-[3-deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/1 rotate-[3-deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             />
           </div>
-          <div>
+          <div className="-mt-20">
             <div className="mx-auto max-w-6xl px-6 lg:px-8">
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="z-10 -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
@@ -64,6 +64,100 @@ export default function Home() {
                     className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
                   />
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-4- -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/1 rotate-[3-deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Feature Section */}
+      <div className="mx-auto  mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-9">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting in Minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Getting Answers from your PDF has never been this easy !
+            </p>
+          </div>
+        </div>
+
+        {/* Steps Section*/}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-violet-700">
+                Step 1
+              </span>
+              <span className="text-xl font-semibold">
+                Sign up for an Account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with a free plan or chose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-violet-700 underline underline-offset-2"
+                >
+                  PRO PLAN
+                </Link>
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-violet-700">
+                Step 2
+              </span>
+              <span className="text-xl font-semibold">
+                Upload your PDF Files
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We will process your file and make it ready to Talk !
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-violet-700">
+                Step 3
+              </span>
+              <span className="text-xl font-semibold">Let&apos;s ball !</span>
+              <span className="mt-2 text-zinc-700">
+                Boom ! Try out HARTdraws Today- it takes just a min to unlock
+                optimization
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        {/* Image Explanation of Steps */}
+
+        <div>
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="mt-16 flow-root sm:mt-24">
+              <div className="z-10 -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <Image
+                  src="/file-upload-preview.jpg"
+                  alt="upload preview"
+                  width={1419}
+                  height={732}
+                  quality={100}
+                  className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                />
               </div>
             </div>
           </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight, Sparkles } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            <span>HARTdraws</span>
+          </Link>
+
+          {/* Mobile Navbar Configuration */}
+
+          <div className="hhidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({ variant: "ghost", size: "sm" })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({ variant: "ghost", size: "sm" })}
+              >
+                Sign In
+              </LoginLink>
+              <RegisterLink className={buttonVariants({ size: "sm" })}>
+                Get Started Today
+                <Sparkles fill="white" className="ml-1.5 w-4" />
+              </RegisterLink>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
